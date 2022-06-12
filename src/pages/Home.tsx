0608911489f0c3fe09428b1682f8a8322a132f73
Adriano Ferreira <piro@outlook.com.br>
2022-06-12 18:07:19 -0300

    Finalização do Desafio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,38 +6,48 @@ import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
 export function Home() {
-  const [tasks, setTasks] = useState<Task[]>([]);
+    const [tasks, setTasks] = useState<Task[]>([]);
 
-  function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
-  }
+    function handleAddTask(newTaskTitle: string) {
+        const task = {
+            id: new Date().getTime(),
+            title: newTaskTitle,
+            done: false,
+        };
+        setTasks((oldTasks) => [...oldTasks, task]);
+    }
 
-  function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
-  }
+    function handleToggleTaskDone(id: number) {
+        const unchangedTasks = tasks.filter((task) => task.id !== id);
+        const [task] = tasks.filter((task) => task.id === id);
 
-  function handleRemoveTask(id: number) {
-    //TODO - remove task from state
-  }
+        const changeTaskDoneStatus = {
+            id: task.id,
+            title: task.title,
+            done: !task.done,
+        };
 
-  return (
-    <View style={styles.container}>
-      <Header tasksCounter={tasks.length} />
+        setTasks([...unchangedTasks, changeTaskDoneStatus]);
+    }
 
-      <TodoInput addTask={handleAddTask} />
+    function handleRemoveTask(id: number) {
+        setTasks((oldState) => oldState.filter((task) => task.id !== id));
+    }
 
-      <TasksList 
-        tasks={tasks} 
-        toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
-      />
-    </View>
-  )
+    return (
+        <View style={styles.container}>
+            <Header tasksCounter={tasks.length} />
+
+            <TodoInput addTask={handleAddTask} />
+
+            <TasksList tasks={tasks} toggleTaskDone={handleToggleTaskDone} removeTask={handleRemoveTask} />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    container: {
+        flex: 1,
+        backgroundColor: '#F4F4F9',
+    },
+});
